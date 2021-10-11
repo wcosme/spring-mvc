004cb4580844d4890bfc8db9cbdbd11b34e22731
Wallace Gonçalves <wallacecosme@gmail.com>
2021-10-11 16:50:26 -0300

    Implementando classes de dominio

--- demo-mvc/src/main/java/br/com/wgsolution/curso/boot/domain/Cargo.java
+++ demo-mvc/src/main/java/br/com/wgsolution/curso/boot/domain/Cargo.java
@@ -1,14 +1,17 @@
 package br.com.wgsolution.curso.boot.domain;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @SuppressWarnings("serial")
 @Entity
-@Table(name = "CARGO")
+@Table(name = "CARGOS")
 public class Cargo extends AbstractEntity<Long> {
 
 	@Column(nullable = false, unique = true, length = 60)
@@ -17,6 +20,9 @@ public class Cargo extends AbstractEntity<Long> {
 	@ManyToOne
 	@JoinColumn(name = "id_departamento_fk")
 	private Departamento departamento;
+	
+	@OneToMany(mappedBy = "cargo")
+	private List<Funcionario> funcionarios;
 
 	public String getNome() {
 		return nome;
@@ -32,5 +38,13 @@ public class Cargo extends AbstractEntity<Long> {
 
 	public void setDepartamento(Departamento departamento) {
 		this.departamento = departamento;
+	}
+
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
 	}
 }
